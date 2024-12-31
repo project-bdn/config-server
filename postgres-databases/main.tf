490b196deb85954e605ef37586dff7ccfc2c2352
Dikongue Samuel <samyfabiol@gmail.com>
2024-12-31 13:50:34 +0100

    merge to new serveur

--- postgres-databases/main.tf
+++ postgres-databases/main.tf
@@ -19,35 +19,17 @@ provider "postgresql" {
   port     = 5432
 }
 
-
-
 module "klivar_play" {
   source        = "./klivar-database-and-user"
   instance_name = "play"
 }
 
-module "klivar_playground" {
-  source        = "./klivar-database-and-user"
-  instance_name = "playground"
-}
-
 # ------------------------------------------
-
 module "klivar_prod" {
   source        = "./klivar-database-and-user"
   instance_name = "prod"
 }
-
-module "klivar_develop" {
-  source        = "./klivar-database-and-user"
-  instance_name = "develop"
-}
-
-
-module "klivar_playg" {
-  source        = "./klivar-database-and-user"
-  instance_name = "playg"
-}
+ 
 # note: the module name is klivar_dev, not klivar_develop as in the outputs.tf file with new version
 module "klivar_dev" {
   source        = "./klivar-database-and-user"
--- postgres-databases/outputs.tf
+++ postgres-databases/outputs.tf
@@ -5,31 +5,11 @@ output "klivar_play" {
   sensitive = true
 }
 
-output "klivar_playground" {
-  value     = module.klivar_playground
-  sensitive = true
-}
-
-# ------------------------------------------
-
 output "klivar_prod" {
   value     = module.klivar_prod
   sensitive = true
 }
-
-# develop actuel
-output "klivar_develop" {
-  value     = module.klivar_develop
-  sensitive = true
-}
-
-# play actuel
-output "klivar_playg" {
-  value     = module.klivar_playg 
-  sensitive = true
-}
-# dev actuel with new version with new version
-
+ 
 output "klivar_dev" {
   value     = module.klivar_dev
   sensitive = true
